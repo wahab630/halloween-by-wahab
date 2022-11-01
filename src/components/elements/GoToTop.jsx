e903ed355225fec83872a32f3336bee57843140a
wahab630 <wahabmalik630@gmail.com>
2022-11-01 19:15:02 +0500

    aa

--- src/components/elements/GoToTop.jsx
+++ src/components/elements/GoToTop.jsx
@@ -1,41 +1,41 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import { FaArrowUp} from "react-icons/fa";
+import { FaArrowUp } from "react-icons/fa";
 
 const GoToTop = () => {
-    const [isVisible , setIsVisible] = useState(false);
+  
+  const [isVisible, setIsVisible] = useState(false);
 
   const topbtn = () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   };
 
-  const listenToScroll = () =>{
-    let heightToHidden =250;
-    const winScroll =document.body.scrollTop || document.documentElement.scrollTop;
+  const listenToScroll = () => {
+    let heightToHidden = 250;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
 
-    if(winScroll > heightToHidden){
-        setIsVisible(true);
-    }
-    else{
-        setIsVisible(false); 
+    if (winScroll > heightToHidden) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
     }
   };
 
- useEffect(()=>{
-    window.addEventListener('scroll',listenToScroll);
-    return () => window.removeEventListener("scroll",listenToScroll);
- },[]);
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
 
   return (
     <>
       <div className="gototop">
-        {isVisible &&  
-        <div className="bottom-btn" onClick={topbtn}>
-        <FaArrowUp  className=""/>
-      </div>
-        }
-        
+        {isVisible && (
+          <div className="bottom-btn" onClick={topbtn}>
+            <FaArrowUp className="" />
+          </div>
+        )}
       </div>
     </>
   );
